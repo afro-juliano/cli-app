01a5c659b4f9d2770aabbb63787892bd03653b2b
afro-juliano <afro.juliano@gmail.com>
2024-02-27 01:54:11 -0300

    Today we add a command to add itens to our todo list

---
+++ cmd/add.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+    "github.com/afro-juliano/cli-app/todo"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new todo",
+	Long: `Add will create a new todo item to the list`,
+    Run: addRun,
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+    items := []todo.Item{}
+    for _, x := range args {
+        items = append(items, todo.Item{Text:x})
+    }
+    fmt.Println(items)
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,31 +1,29 @@
 package cmd
 
 import (
+    "fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+    //"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "cli-app",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "cli-app is a todo application",
+	Long: `cli-app will help you get more done in less time.
+            It's designed to be as simple as possible to Help
+            you acomplish your goals`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("cli-app is good")
+    },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		os.Exit(1)
+        fmt.Println(err)
+		os.Exit(-1)
 	}
 }
 
